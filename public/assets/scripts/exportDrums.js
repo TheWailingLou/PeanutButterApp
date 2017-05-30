00591be2305b45c3c091f26fd1823640e4628810
Louis Antweiler <Louis.Antweiler@Gmail.com>
2017-05-29 21:43:18 -0600

    re-added drum export file to repo

---
+++ public/assets/scripts/exportDrums.js
@@ -0,0 +1,96 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var context = new AudioContext();
+const peanutButter = require('../Sound_Generator/build/Release/peanutButter.node')
+
+function formatBuffer(audioBuffer) {
+  var formattedBufferArray = []
+  for (var channel=0; channel<audioBuffer.numberOfChannels; channel++) {
+    formattedBufferArray.push(audioBuffer.getChannelData(channel))
+  }
+  return formattedBufferArray
+}
+
+function loadSound(url) {
+  var request = new XMLHttpRequest()
+  request.open('GET', url, true)
+  request.responseType = 'arraybuffer';
+
+  let audioPromise = new Promise(function(resolve, reject) {
+    request.onload = function() {
+      context.decodeAudioData(request.response, function(buffer) {
+        soundBuffer = formatBuffer(buffer);
+        resolve(soundBuffer);
+      })
+    }
+  })
+  request.send()
+  return audioPromise
+}
+
+Promise.all([
+  loadSound('./assets/mp3/crash.mp3'),
+  loadSound('./assets/mp3/crash2.mp3'),
+  loadSound('./assets/mp3/hi-hat-closed.mp3'),
+  loadSound('./assets/mp3/hi-hat-open.mp3'),
+  loadSound('./assets/mp3/kick.mp3'),
+  loadSound('./assets/mp3/ride.mp3'),
+  loadSound('./assets/mp3/ride2.mp3'),
+  loadSound('./assets/mp3/snare.mp3'),
+  loadSound('./assets/mp3/snareSoft.mp3'),
+  loadSound('./assets/mp3/tomFloor.mp3'),
+  loadSound('./assets/mp3/tomFloor2.mp3'),
+  loadSound('./assets/mp3/tomHigh.mp3'),
+  loadSound('./assets/mp3/tomMid.mp3'),
+  loadSound('./assets/mp3/tomMidHigh.mp3')
+]).then(function(bufferArray){
+  var crash1Buff = bufferArray[0]
+  var crash2Buff = bufferArray[1]
+  var hiHatClosedBuff = bufferArray[2]
+  var hiHatOpenBuff = bufferArray[3]
+  var kickBuff = bufferArray[4]
+  var ride1Buff = bufferArray[5]
+  var ride2Buff = bufferArray[6]
+  var snareBuff = bufferArray[7]
+  var snareSoftBuff = bufferArray[8]
+  var tomFloor1Buff = bufferArray[9]
+  var tomFloor2Buff = bufferArray[10]
+  var tomHighBuff = bufferArray[11]
+  var tomMidBuff = bufferArray[12]
+  var tomMidHighBuff = bufferArray[13]
+
+  peanutButter.setCrash1(crash1Buff)
+  peanutButter.setCrash2(crash2Buff)
+  peanutButter.setHiHatOpen(hiHatOpenBuff)
+  peanutButter.setHiHatClosed(hiHatClosedBuff)
+  peanutButter.setKick(kickBuff)
+  peanutButter.setRide1(ride1Buff)
+  peanutButter.setRide2(ride2Buff)
+  peanutButter.setSnare(snareBuff)
+  peanutButter.setSnareSoft(snareSoftBuff)
+  peanutButter.setTomFloor1(tomFloor1Buff)
+  peanutButter.setTomFloor2(tomFloor2Buff)
+  peanutButter.setTomHigh(tomHighBuff)
+  peanutButter.setTomMid(tomMidBuff)
+  peanutButter.setTomMidHigh(tomMidHighBuff)
+
+  peanutButter.setSampleRate(44100)
+  peanutButter.setTempo(120)
+
+  var drumTrack = peanutButter.getMainDrumBuffer()
+  var drumBuffer = context.createBuffer(drumTrack.length, drumTrack[0].length, 44100)
+  drumTrack.forEach(function(channel, j) {
+    var buffSize = channel.length
+    var float32 = new Float32Array(buffSize)
+    for (var i=0; i<buffSize; i++)
+    {
+      float32[i] = channel[i]
+    }
+    drumBuffer.copyToChannel(float32, j)
+  })
+  var source = context.createBufferSource()
+  source.buffer = drumBuffer
+  source.connect(context.destination);
+  soure.start()
+  
+})
