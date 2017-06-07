9e722a01920f6acc5ea606359e972b4684543d24
Louis Antweiler <Louis.Antweiler@Gmail.com>
2017-06-06 23:29:30 -0600

    Add arrangement ending

--- Sound_Generator/readers/bass/bass_writer.cc
+++ Sound_Generator/readers/bass/bass_writer.cc
@@ -30,4 +30,28 @@ namespace bass_writer
     }
     std::cout << "past the bass_bar" << std::endl;
   }
+
+  void final_note(int note, int duration, int octave, int bar)
+  {
+    std::cout << "\nmaking into the final note" << std::endl;
+    int lowest_of_note = notes::find_lowest_note(note);
+    int actual_note = lowest_of_note + (octave*12);
+    int frame_duration = audio_helper::teenth_slice_as_frame(duration);
+    int frame_location = audio_helper::bar_duration_in_frames() * bar;
+
+    std::cout << "\n\nGETTING ALL DATA. SENDING TO fourier_tone " << std::endl;
+
+
+    // bass_voice::write_note_at_location(note, teenth_duration, i, bar);
+    // bass_voice::super_square_at_location(note, frame_duration, frame_location);
+    // bass_voice::triangle_at_location(note, frame_duration, frame_location);
+    // bass_voice::sine_at_location(note, frame_duration, frame_location);
+    // bass_voice::half_sine_at_location(note, frame_duration, frame_location);
+    // bass_voice::half_cos_at_location(note, frame_duration, frame_location);
+    bass_voice::fourier_tone(actual_note, frame_duration, frame_location);
+    // bass_voice::fourier_tone_2(note, frame_duration, frame_location);
+    // bass_voice::fourier_tone_3(note, frame_duration, frame_location);
+
+    // bass_voice::tone_play(note, frame_duration, frame_location);
+  }
 }
